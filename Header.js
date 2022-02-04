03fb588545dcbe2c8b29a6afa4e1ba8585225893
sc0ut0P <95422255+sc0ut0P@users.noreply.github.com>
2022-02-04 13:24:06 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,16 @@
+import react from "react"
+import ReorderIcon from '@material-ui/icons/Reorder';
+import { Icon, IconButton } from "@material-ui/core";
+Import {IconButton} from '@material-ui/core'
+
+const Header = ()->{
+    return(
+        <div className="header">
+        <div className="header_left"
+        <ReorderIcon></ReorderIcon>
+        </IconButton>
+        <img src="https://tse3.mm.bing.net/th?id=OIP.noMBeYstQbWVmRNL37QXZQHaE8&pid=Api&P=0&w=248&h=165" alt="logo"/>
+        </div>
+    )
+}
+export default Header
